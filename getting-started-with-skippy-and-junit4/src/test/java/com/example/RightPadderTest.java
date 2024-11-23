2f7c19a5ebbfa1f6df490b0e9dffa85d0bf3e294
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-11-22 19:41:03 -0600

    catch up with latest in 0.0.24

--- getting-started-with-skippy-and-junit4/src/test/java/com/example/RightPadderTest.java
+++ getting-started-with-skippy-and-junit4/src/test/java/com/example/RightPadderTest.java
@@ -1,10 +1,16 @@
 package com.example;
 
+import io.skippy.junit4.Skippy;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
 import static org.junit.Assert.assertEquals;
 
 public class RightPadderTest {
+
+    @Rule
+    public TestRule skippyRule = Skippy.predictWithSkippy();
 
     @Test
     public void testPadRight() {
