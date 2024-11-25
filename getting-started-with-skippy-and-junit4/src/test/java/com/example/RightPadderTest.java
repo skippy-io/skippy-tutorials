a432c9c789795e7b1a7a034547d4b9f0dfd26b60
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-11-25 17:02:11 -0600

    issues/189: poc for exclusions

--- getting-started-with-skippy-and-junit4/src/test/java/com/example/RightPadderTest.java
+++ getting-started-with-skippy-and-junit4/src/test/java/com/example/RightPadderTest.java
@@ -1,16 +1,10 @@
 package com.example;
 
-import io.skippy.junit4.Skippy;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
 
 import static org.junit.Assert.assertEquals;
 
 public class RightPadderTest {
-
-    @Rule
-    public TestRule skippyRule = Skippy.predictWithSkippy();
 
     @Test
     public void testPadRight() {
